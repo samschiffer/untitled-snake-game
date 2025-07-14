b044d6ed367fdeebd62953c553095ac54fc0db2f
samschiffer <66092042+samschiffer@users.noreply.github.com>
2025-07-14 11:25:23 -0400

    Refactor code into folders and cleanup

---
+++ Alternate Train/README.txt
@@ -0,0 +1,6 @@
+These scenes were used for experimenting with how to make the train.
+
+There are a few different implementations of the train car movement in new_train.gd
+that make the train move in different ways
+
+The implementation that should be used is train.gd
--- Characters/Enemy/enemy.gd
+++ Characters/Enemy/enemy.gd
@@ -4,32 +4,35 @@ signal died
 
 @export var shoot_cooldown := 1
 @export var health := 10
-@export var bullet_scene: PackedScene = preload("res://shuriken.tscn")
+@export var bullet_scene: PackedScene = preload("res://Objects/shuriken.tscn")
 
-@onready var Player_Train := get_tree().get_first_node_in_group("Trains")
+@onready var player_train := get_tree().get_first_node_in_group("Trains")
 
-var colliding_cars: Array[Node] = []
-
+var colliding_cars: Array[Area2D] = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$ShootTimer.start()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Unpause the shoot timer if there are no cars attacking (colliding with) the enemy
 	if colliding_cars.size() == 0:
 		$ShootTimer.set_paused(false)
+	
+	# Update the health label
 	$Label.text = str(health)
 	$Label2.text = String.num(snapped($ShootTimer.time_left, 0.1))
+	
+	# Debugging the shoot function
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-	if Player_Train:
-		rotation = global_position.direction_to(Player_Train.get_node("Locomotive").global_position).angle() + PI / 2
-	pass
 	
+	# Rotate towards the train's locomotive if it exists
+	if player_train:
+		rotation = global_position.direction_to(player_train.get_node("Locomotive").global_position).angle() + PI / 2
 
 
 func _on_timer_timeout() -> void:
@@ -49,16 +52,15 @@ func _on_area_entered(area: Area2D) -> void:
 		$ShootTimer.set_paused(true)
 		hit()
 
+
 func _on_area_exited(area: Area2D) -> void:
 	if area is Locomotive or area is TrainCar:
 		colliding_cars.remove_at(colliding_cars.find(area))
 
 
 func hit():
-	#$ShootTimer.stop()
 	health -= 1
 	if health <= 0:
-		#print("Enemy Dead")
 		die()
 
 
