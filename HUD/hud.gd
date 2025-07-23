debd387fb11dfa32070147e4867dde3c023f51fb
samschiffer <66092042+samschiffer@users.noreply.github.com>
2025-07-22 22:13:57 -0400

    Add objectives and levels

--- HUD/hud.gd
+++ HUD/hud.gd
@@ -2,12 +2,25 @@ extends CanvasLayer
 
 signal start_game
 
+# Health variables
 var max_health: float = 100
 @onready var initial_health_bar_width = $HealthLabel/ColorRect.size.x
+
+# Objective variables
+var current_objective: String
+var objective_goal: String
+var objective_progress: String
+var objective_name_map := {
+	"enemies": "Defeat Enemies:",
+	"time": "Survive for",
+	"score": "Score Points:"
+}
 
 func _ready() -> void:
 	$HealthLabel.hide()
 	$ScoreLabel.hide()
+	$ObjectiveLabel.hide()
+	$LevelLabel.hide()
 
 
 func _on_start_button_pressed() -> void:
@@ -24,6 +37,8 @@ func start_pushed():
 	$Message.hide()
 	$HealthLabel.show()
 	$ScoreLabel.show()
+	$ObjectiveLabel.show()
+	$LevelLabel.show()
 	start_game.emit()
 
 
@@ -34,6 +49,8 @@ func show_game_over():
 	$Message.text = "Play Again?"
 	$HealthLabel.hide()
 	$ScoreLabel.hide()
+	$ObjectiveLabel.hide()
+	$LevelLabel.hide()
 	$StartButton.show()
 
 
@@ -46,4 +63,21 @@ func update_current_health(current_health: float):
 
 
 func update_score(score: int):
-	$ScoreLabel/Score.text = str(score)
+	$ScoreLabel.text = "Score: " + str(score)
+
+func update_objective(objective: String):
+	current_objective = objective
+
+func update_objective_progress(progress, goal = null):
+	objective_progress = str(progress)
+	if goal != null:
+		objective_goal = str(goal)
+	
+	if current_objective == "time":
+		$ObjectiveLabel.text = objective_name_map[current_objective] + " " + objective_progress + "s"
+	else:
+		$ObjectiveLabel.text = objective_name_map[current_objective] + " " + objective_progress + "/" + objective_goal
+
+
+func update_level(level):
+	$LevelLabel.text = "Level: " + str(level)
