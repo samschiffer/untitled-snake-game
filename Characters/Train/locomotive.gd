9c639e940759d66da1dfee457918a7a1f97e7308
samschiffer <66092042+samschiffer@users.noreply.github.com>
2025-08-10 20:54:32 -0400

    Add boost to train

--- Characters/Train/locomotive.gd
+++ Characters/Train/locomotive.gd
@@ -12,18 +12,30 @@ const FRICTION: float = 2
 const WALL_COLLISION_SPEED = 90
 
 var speed: float = 0
+var max_speed
+const MAX_SPEED_BASE = 600
+const MAX_SPEED_BOOST = 800
 var screen_size: Vector2
 var angular_speed: float = PI
+
+var boost_amount
+const MAX_BOOST_AMOUNT = 100
 
 var movement_locked: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	max_speed = MAX_SPEED_BASE
+	boost_amount = MAX_BOOST_AMOUNT
 	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Debug labels for speed and boost
+	$SpeedLabel.text = str(speed)
+	$BoostLabel.text = str(boost_amount)
+	
 	if independent:
 		move(delta)
 
@@ -32,13 +44,29 @@ func move(delta: float):
 	if not movement_locked:
 		# Decrease speed based on friction
 		speed -= FRICTION
-		# Increase or decrease speed
+		
+		# Increase or decrease speed and add boost
 		if Input.is_action_pressed("move_up"):
-			speed += 5
+			if Input.is_action_pressed("boost") and boost_amount > 0:
+				boost_amount -= 3
+				speed += 15
+				max_speed = MAX_SPEED_BOOST
+			else: 
+				if speed < MAX_SPEED_BASE:
+					speed += 5
+					max_speed = MAX_SPEED_BASE
+				elif speed >= MAX_SPEED_BASE:
+					speed -= 5
 		if Input.is_action_pressed("move_down"):
 			speed -= 5
+		if not Input.is_action_pressed("boost"):
+			boost_amount += 1
+		
+		# Clamp boost
+		boost_amount = clamp(boost_amount, 0, MAX_BOOST_AMOUNT)
+		
 		# Clamp speed
-		speed = clamp(speed, 0, 600)
+		speed = clamp(speed, 0, max_speed)
 		
 		# Set angular speed proportional to speed and cap at PI
 		angular_speed = PI * (speed / 400.0)
